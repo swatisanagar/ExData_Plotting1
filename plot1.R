0982a97f0a5e0abdcd061af2e14eea4916a37720
swatisanagar <swatisanagar@yahoo.co.in>
2015-09-13 21:07:18 +0530

    Modifications

--- plot1.R
+++ plot1.R
@@ -1,8 +1,11 @@
+#Global Active Power Vs Frequency plot
 dataFile <- "../data/household_power_consumption.txt"
+#Read data
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot1.png", width=480, height=480)
+#Plot a histogram
 hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
 dev.off() 
