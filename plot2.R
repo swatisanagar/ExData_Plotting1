f3c916a4d8b16512d9e58d24cd2227de0b9a61c6
swatisanagar <swatisanagar@yahoo.co.in>
2015-09-13 21:36:28 +0530

    Modifications

--- plot2.R
+++ plot2.R
@@ -1,7 +1,9 @@
+#Change in 2 day period
 dataFile <- "../data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+#subsettting data
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
+#necessary conversions
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
--- plot4.R
+++ plot4.R
@@ -1,7 +1,8 @@
+#All plots together
 dataFile <- "../data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
-#str(subSetData)
+#
 datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 globalActivePower <- as.numeric(subSetData$Global_active_power)
 globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
@@ -9,6 +10,7 @@ voltage <- as.numeric(subSetData$Voltage)
 subMetering1 <- as.numeric(subSetData$Sub_metering_1)
 subMetering2 <- as.numeric(subSetData$Sub_metering_2)
 subMetering3 <- as.numeric(subSetData$Sub_metering_3)
+#plotting
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2))
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
